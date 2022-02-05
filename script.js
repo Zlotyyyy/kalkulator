444d672a2ad062f276ac41e03cd76f08ea5a81e8
Zlotyyyy <95238530+Zlotyyyy@users.noreply.github.com>
2022-02-05 21:14:56 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+var liczba1 = 0;
+var liczba2 = 0;
+var rodzajDzialania = 0;
+var wynik = 0;
+var poleWynik = document.getElementById("wynik");
+
+
+
+function wpisz(cyfra) {
+    if(poleWynik.innerHTML == "NAN" || poleWynik.innerHTML == "Error") {
+        poleWynik.innerHTML = "";
+    };
+    poleWynik.innerHTML = poleWynik.innerHTML + cyfra;
+};
+
+function dzialanie(symbol) {
+    if(poleWynik.innerHTML !== "") {
+    liczba1 = Number(poleWynik.innerHTML);
+    };
+    poleWynik.innerHTML = "";
+    rodzajDzialania = symbol;
+    document.getElementById("dzialanie").innerHTML = symbol;
+};
+
+function rownanie() {
+    liczba2 = Number(poleWynik.innerHTML);
+    wynik = liczba1 + rodzajDzialania + liczba2;
+    poleWynik.innerHTML = eval(wynik);
+    document.getElementById("dzialanie").innerHTML = "";
+    if(liczba2 == 0 && rodzajDzialania == "/") {
+        poleWynik.innerHTML = "Error";
+    };
+};
+
+function wyczysc() {
+    poleWynik.innerHTML = "";
+};
